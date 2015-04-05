7e89f5ee6e4cd895c55d3523ec6411cdc6dd5d24
Roderic <marilu@marilus-macbook.local>
2015-04-04 17:20:05 -0700

    Signed-off-by: Roderic <marilu@marilus-macbook.local>

---
+++ Hmwk/HW_Chap_10_Pr_4/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <iomanip>
+using namespace std;
+
+int main (int argc, char * const argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+	cout << "Don't try this at home";
+    return 0;
+}
