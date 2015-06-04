1c715e8328334e50570ba349b534c3452b6b63fb
Roderic1 <rrodriguez171@student.rcc.edu>
2015-06-04 15:21:58 -0700

    c1
    c1

--- Hmwk/Asm 3/Ch_12_Pr_7/main.cpp
+++ Hmwk/Asm 3/Ch_12_Pr_7/main.cpp
@@ -21,20 +21,52 @@ int main(int argc, char** argv) {
     string inFile;          //Name of file of incoming sentences
     string oFile;           //Name of file to write sentences
     fstream witFile;        //File string object: Input
-    ostream outFile;        //File string object: Output
+    ofstream outFile;       //File string object: Output
+    char ch;                //Character to transfer everything over
     
     //Get the file name
     cout<<"Enter in the name of the file you wish to open."<<endl;
     cin>>inFile;
     
     //Open the file
-    witFile.open(infile.c_str(),ios::in);
+    
+    cout<<inFile;
     
     
+    cin.ignore();
+    
+    //Get the output file name
+    cout<<"Enter in the name of the file to input the information."<<endl;
+    cin>>oFile;
+    cin.ignore();
+    cout<<"now transfering.";
+    
+    witFile.open(inFile.c_str(),ios::in);
+    outFile.open(oFile.c_str(),ios::out);
+    
+    //If the file is successfully opened, continue
+    if(witFile){
+        witFile.get(ch);
+        while(witFile){
+            if(ch == '.'){
+                outFile.put(ch);
+                outFile.put('n');
+            }
+            else
+                outFile.put(ch);
+        }
+        
+    }
+    else
+        cout<<"The in file was unable to open";
     
     
+    cout<<"Transfer is complete";
     
     
+    //Close the files
+    witFile.close();
+    outFile.close();
 
     return 0;
 }
